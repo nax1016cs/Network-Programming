a4e3bcd1f730103ad20d888af496be8f2b2a20f4
cmchiang0516097 <nax1016@gmail.com>
2020-04-07 14:35:01 +0800

    finish

--- database.py
+++ database.py
@@ -2,37 +2,49 @@ import sqlite3
 import sys
 # global conn,c
 
-conn = sqlite3.connect('hw1.db')
-c = conn.cursor()
+
 
 class database():
     def __init__(self):
-        self.create()
-
-    def create(self):
-       try:
-         c.execute('''CREATE TABLE user
-              ( UID INTEGER PRIMARY KEY AUTOINCREMENT,
-                Username TEXT NOT NULL UNIQUE,
-                Email TEXT NOT NULL,
-                Password TEXT NOT NULL );''')
-       except sqlite3.OperationalError:
-         pass
-
-    def insert(self, name, email, password):
-       try:
-         c.execute("INSERT INTO user ( Username , Email , Password) \
-             VALUES (?, ?, ? )" , (name, email, password ))
-       except sqlite3.IntegrityError:
-         pass
-
-    def delete(self):
-       c.execute("DELETE  from user")
-
-    def close_db(self):
-        
+        conn = sqlite3.connect('hw1.db')
+        c = conn.cursor()
+        try:
+            c.execute('''CREATE TABLE user
+                      ( UID INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Username TEXT NOT NULL UNIQUE,
+                        Email TEXT NOT NULL,
+                        Password TEXT NOT NULL );''')
+        except sqlite3.OperationalError:
+            pass
         conn.commit()
         conn.close()
 
-if __name__ == "__main__":
-    pass
+    def insert(self, name, email, password, socket):
+        conn = sqlite3.connect('hw1.db')
+        c = conn.cursor()
+        try:
+            c.execute("INSERT INTO user ( Username , Email , Password) \
+                   VALUES (?, ?, ? )" , (name, email, password ))
+            socket.send("Register successfully.\n\r".encode())
+
+        except sqlite3.IntegrityError:
+            socket.send("Username is already used.\n\r".encode())
+        conn.commit()
+        conn.close()
+
+    def select(self, name, password, socket):
+        conn = sqlite3.connect('hw1.db')
+        c = conn.cursor()
+        cursor = c.execute("SELECT COUNT(*) FROM user WHERE Username = (?) AND Password = (?)", (name, password,))
+        for row in cursor:
+            return row[0]
+        conn.commit()
+        conn.close()
+
+    def delete(self):
+        conn = sqlite3.connect('hw1.db')
+        c = conn.cursor()
+        c.execute("DELETE from user")
+        conn.commit()
+        conn.close()
+ 
--- hw1.py
+++ hw1.py
@@ -2,8 +2,9 @@ import socket
 import threading
 from database import database
 import sqlite3
+import sys
 
-
+db = database()
 
 class thread_server(threading.Thread):
 
@@ -11,41 +12,98 @@ class thread_server(threading.Thread):
         threading.Thread.__init__(self)
         self.socket = socket
         self.addr = addr
+        self.user = ""
 
     def run(self):
-        self.socket.send("********************************\n\r")
-        self.socket.send("** Welcome to the BBS server. **\n\r")
-        self.socket.send("********************************\n\r")
+
+        conn = sqlite3.connect('hw1.db')
+        c = conn.cursor()
+        # db.delete()
+        self.socket.send("********************************\n\r".encode())
+        self.socket.send("** Welcome to the BBS server. **\n\r".encode())
+        self.socket.send("********************************\n\r".encode())
 
         while True:
-            data = self.socket.recv(1024)
-            if data.decode() == 'exit':
-                break  
-            print('recive:',data.decode())
-            # self.socket.send(data.upper())
+            self.socket.send("% ".encode())
+            data_in = self.socket.recv(1024)
+            
+            if not data_in:
+                continue
+            elif data_in.decode() == 'exit\r\n':
+                break
+
+            data = data_in.decode().split()
+
+            if(data[0] == "register"):
+            	if( len(data) != 4):
+            		self.socket.send("Usage: register <username> <email> <password>\n\r".encode())
+            	else:
+            		db.insert(data[1], data[2], data[3], self.socket)
+            
+            elif(data[0] == "login"):
+
+                if( len(data) != 3):
+                    self.socket.send("login <username> <password>\n\r".encode())
+                elif(len(self.user) == 0):
+
+                    count = db.select(data[1], data[2], self.socket)
+                    if(count == 0):
+                        self.socket.send("Login failed.\n\r".encode())
+
+                    elif(count == 1):
+                        welcome_str = "Welcome, " + data[1] + ".\n\r"
+                        self.socket.send(welcome_str.encode())
+                        self.user = data[1]
+                else:
+                    self.socket.send("Please logout first.\n\r".encode())
+
+            elif( data[0] == "logout" and len(data) == 1):
+
+                if( len(self.user) == 0):
+                    self.socket.send("Please login first.\n\r".encode())
+                else:
+                    bye_str = "Bye, " + self.user + ".\n\r"
+                    self.socket.send(bye_str.encode())
+                    self.user = ""
+
+            elif( data[0] == "whoami" and len(data) == 1):
+
+                if( len(self.user) == 0):
+                    self.socket.send("Please login first.\n\r".encode())
+                else:
+                    user_str = self.user + ".\n\r"
+                    self.socket.send(user_str.encode())
+
+            else:
+                pass
+            # data = data_in.decode(encoding='utf-8').split()
+            print("recieve", data)
+            # print('recive:',data_in.decode())
+            # self.socket.send(data_in.upper())
         self.socket.close()
+        conn.commit()
+        conn.close()
+        return
 
 
 if __name__ == "__main__":
+
+    port = 9090
+    if( len(sys.argv) == 2):
+        port = int(sys.argv[1])
     
-    # db = database() 
-    # db.delete()
-    # db.create()
-    # db.insert('Teddy', '123' , '456')
-    # db.insert('BOB', '7897' , 'dsfaf')
 
 
     # cursor = c.execute("SELECT Username, Email, Password   FROM user ")
     # # print(cursor)
     # for row in cursor:
-    #     print("Username", row[0])
-    #     print("Email", row[1])
-    #     print("Password", row[2])
+    #    print("Username", row[0])
+    #    print("Email", row[1])
+    #    print("Password", row[2])
 
 
     server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     host = socket.gethostname()
-    port = 9090
     server.bind((host,port)) 
     # print(host, port)
     server.listen(10) 
